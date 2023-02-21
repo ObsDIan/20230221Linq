a752d624fcfd562d27eea31ce9d0d250177eec69
ObsDIan <a29879350@gmail.com>
2023-02-21 13:18:33 +0800

    3

---
+++ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            foreach (var item in Enumerable.Range(1, n).ToList())
+            {
+                Console.WriteLine(string.Join("", Enumerable.Repeat((n + 1) - item, item).ToList()));
+            }
+            Console.ReadLine();
+        }
+    }
+}
